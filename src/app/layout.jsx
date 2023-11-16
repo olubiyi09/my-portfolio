b20e9cfb4d77a8a2beeb2bb7243875b54847af03
olubiyi09 <olubiyi09@gmail.com>
2023-11-16 06:07:27 +0400

    fix the loader bug

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,99 +1,24 @@
-"use client"
-
-import React, { useState, useEffect } from 'react';
-import { Montserrat } from 'next/font/google';
 import "./globals.css";
+import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['200', '300', '400', '500', '600', '700', '800'],
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
-const Loader = () => {
-  return (
-    <div className="loader ">
-      {/* Customize your loader content here */}
-      <img src="loading.svg" alt='Loading...' />
-    </div>
-  );
-};
-
 export const metadata = {
-  title: 'SeyiDev',
-  description: 'My Portfolio Website',
+  title: "SeyiDev",
+  description: "My Portfolio Website",
 };
 
 export default function RootLayout({ children }) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate a delay in loading (e.g., 3 seconds)
-    const timeout = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-
-    // Clear the timeout to avoid memory leaks
-    return () => clearTimeout(timeout);
-  }, []); // Empty dependency array means this effect runs once on mount
-
   return (
     <html lang="en">
       <body className={montserrat.className}>
         <div className="h-screen">
-          {loading ? (
-            <div className="bg flex justify-center items-center h-full">
-
-              <Loader />
-            </div>
-          ) : (
-            <div className="main">{children}</div>
-          )}
+          <div className="main">{children}</div>
         </div>
       </body>
     </html>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import "./globals.css";
-// import { Montserrat } from "next/font/google";
-
-// const montserrat = Montserrat({
-//   subsets: ["latin"],
-//   weight: ["200", "300", "400", "500", "600", "700", "800"],
-// });
-
-// export const metadata = {
-//   title: "SeyiDev",
-//   description: "My Portfolio Website",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className={montserrat.className}>
-//         <div className="h-screen">
-//           <div className="main">{children}</div>
-//         </div>
-//       </body>
-//     </html>
-//   );
-// }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,51 @@
-import "./globals.css";
-import Navbar from "./components/Navbar";
-import IntroSection from "./components/IntroSection";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+"use client"
+
+import React, { useState, useEffect } from 'react';
+import './globals.css';
+import Navbar from './components/Navbar';
+import IntroSection from './components/IntroSection';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+const Loader = () => {
+  return (
+    <div className="loader bg flex justify-center items-center h-screen w-screen">
+      <img src="loading.svg" alt="Loading..." />
+    </div>
+  );
+};
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <section>
-      <div>
-        <Navbar />
-        <IntroSection />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-        <Footer />
-      </div>
+      {loading ? (
+        <div className="bg flex justify-center items-center h-screen w-screen">
+          <Loader />
+        </div>
+      ) : (
+        <div>
+          <Navbar />
+          <IntroSection />
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+          <Footer />
+        </div>
+      )}
     </section>
   );
 };
